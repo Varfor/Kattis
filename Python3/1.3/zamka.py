dffcd22b419ffdc93e4aa3d9e22ef81d19c7e7fe
Varfor <38240861+Varfor@users.noreply.github.com>
2018-08-01 10:27:49 +0200

    solution

--- Python3/1.3/zamka.py
+++ Python3/1.3/zamka.py
@@ -3,11 +3,11 @@ d = int(input())
 x = int(input())
 utmin = int()
 utmax = int()
-for i in range(l, d):
+for i in range(l, d+1):
 	if(sum([int(x) for x in str(i)]) == x):
 		utmin = i
 		break
-for i in range(d, l, -1):
+for i in range(d, l-1, -1):
 	if(sum([int(x) for x in str(i)]) == x):
 		utmax = i
 		break
