981d4a191df1a6234d862c3f30a8082010ac3ca2
Varfor <38240861+Varfor@users.noreply.github.com>
2018-10-09 13:59:39 +0200

    solution

---
+++ Python3/2.0/aboveaverage.py
@@ -0,0 +1,4 @@
+for _ in range(int(input())):
+    a = [int(x) for x in input().split()[1:]]
+    avg = sum([x for x in a])/len(a)
+    print('{0:.3f}%'.format(len([x for x in a if x>avg])/len(a)*100) )
