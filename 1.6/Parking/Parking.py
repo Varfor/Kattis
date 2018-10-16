f3303d9cfd5bfcb44b4696e1540dbe96af6fea7b
Varfor <38240861+Varfor@users.noreply.github.com>
2018-10-16 10:02:49 +0200

    solution

--- 1.6/Parking/Parking.py
+++ 1.6/Parking/Parking.py
@@ -1,20 +1,24 @@
 price = [int(x) for x in input().split()]
 arrive = []
 depart = []
-bil = -1
+bil = 0
 ut = 0
 for _ in range(3):
     a, b = [int(x) for x in input().split()]
     arrive.append(a)
     depart.append(b)
+arrive = sorted(arrive)
+depart = sorted(depart)
 prev = min(arrive)
-for x in range(min(arrive), max(depart)):
-    if x in arrive or x in depart:
+for x in range(min(arrive), max(depart)+1):
+    if bil>=0:
+        if min(arrive) == x:
+            continue
+        elif x in arrive or x in depart:
+            ut += (abs(x-prev))*price[bil]*(bil+1)
+            bil += arrive.count(x)-depart.count(x)
+            prev = x
+    elif bil==-1 and x in arrive:
         bil += arrive.count(x)-depart.count(x)
-        print(ut, bil)
         prev = x
-        print('value:', x,'price[bil]:', price[bil])
-    if:
-        ut += abs(x-prev)*price[bil]
-        # print(arrive.count(x), depart.count(x), price[bil])
 print(ut)
